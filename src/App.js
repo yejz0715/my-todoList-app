f7107d623cc1d89cbe9ce508ed695595914096ca
yejz0715 <yejz0715@gmail.com>
2023-02-17 23:42:34 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return;
-}
+import SignIn from "./pages/Auth/SignIn";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <SignIn />
+    </div>
+  );
+}
+//  2/16
+//1. 라우터
+//2. signin, signup (심플하게) + 이메일 그거
+//3. todo 나오게
+
+//  2/17
+//4. api+store+reducer 틀만 만들기
 export default App;
