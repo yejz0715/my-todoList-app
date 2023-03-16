afa0cff2f274fccc22fd817bd1bd3ba198c78657
yejz0715 <yejz0715@gmail.com>
2023-03-16 22:46:55 +0900

    Add:add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import SignIn from "./pages/Auth/SignIn";
+import AppRouter from "./AppRouter";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <SignIn />
+      <AppRouter />
     </div>
   );
 }
-//  2/16
-//1. 라우터
-//2. signin, signup (심플하게) + 이메일 그거
-//3. todo 나오게
-
-//  2/17
-//4. api+store+reducer 틀만 만들기
 export default App;
