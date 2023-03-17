3833a7e97ad130810ab3a55dead2f257dc091d5d
yejz0715 <yejz0715@gmail.com>
2023-03-17 23:36:30 +0900

    Add:userSlice

---
+++ src/modules/User.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initState = {
+  users: [],
+};
+const userSlice = createSlice({
+  name: "userReducer",
+  initialState: initState,
+  reducers: {
+    signUp: (state, { payload }) => {
+      state.users.push({
+        userId: payload.userId,
+        userPassword: payload.userPassword,
+      });
+    },
+    logIn: (state, { payload }) => {
+      state.users = state.users.filter((user) => user.userId === payload);
+    },
+  },
+});
+export const { logIn, signUp } = userSlice.actions;
