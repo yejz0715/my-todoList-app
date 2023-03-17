b3e85c5920f87574365d04c21f614085437b3598
yejz0715 <yejz0715@gmail.com>
2023-03-17 23:35:50 +0900

    Add:Component_header + style

---
+++ src/components/Common/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./style";
+
+const Header = () => {
+  return (
+    <S.Container>
+      <S.Title>툳</S.Title>
+    </S.Container>
+  );
+};
+export default Header;
---
+++ src/components/Common/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Title = styled.label`
+  font-size: 5rem;
+  margin: 5rem;
+`;
