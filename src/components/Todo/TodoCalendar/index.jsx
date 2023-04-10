39707a1b2abf32a06565b5a3600fa499ae4b622a
yejz0715 <yejz0715@gmail.com>
2023-04-10 23:08:25 +0900

    todoCalendar

---
+++ src/components/Todo/TodoCalendar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "react-calendar";
+import * as S from "./style";
+const TodoCalendar = () => {
+  return (
+    <S.Container>
+      <Calendar />
+    </S.Container>
+  );
+};
+
+export default TodoCalendar;
