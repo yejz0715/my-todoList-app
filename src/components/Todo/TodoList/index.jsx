18faecdf312a5c166897990e27d99b5fadeab34a
yejz0715 <yejz0715@gmail.com>
2023-04-15 01:28:15 +0900

    components_todolist

---
+++ src/components/Todo/TodoList/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+import TodoItem from "../TodoItem";
+const TodoList = () => {
+  return (
+    <S.Container>
+      <TodoItem />
+    </S.Container>
+  );
+};
+
+export default TodoList;
---
+++ src/components/Todo/TodoList/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 28.625rem;
+  height: 100%;
+  max-height: 33rem;
+  background-color: white;
+`;
