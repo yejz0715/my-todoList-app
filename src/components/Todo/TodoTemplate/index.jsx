6694f5603669bfa9292c340755603f473892a9cf
yejz0715 <yejz0715@gmail.com>
2023-04-15 01:29:27 +0900

    components_todoTemplate

---
+++ src/components/Todo/TodoTemplate/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as S from "./style";
+const TodoTemplate = ({ children }) => {
+  return <S.Container>{children}</S.Container>;
+};
+
+export default TodoTemplate;
