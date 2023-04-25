14a94a4e7867b7badee8360a0d59158f6a0cbb4b
yejz0715 <yejz0715@gmail.com>
2023-04-25 21:59:27 +0900

    todoInputModal

--- src/components/Todo/TodoInput/index.jsx
+++ src/components/Todo/TodoInput/index.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import * as S from "./style";
-const TodoInput = () => {
+const TodoInput = ({ setInputModalOpen }) => {
+  const handleCloseModal = () => {
+    setInputModalOpen(false);
+  };
+
+  const handleAddTodoContent = () => {
+    handleAddTodoContent(false);
+  };
+
   return (
     <S.Container>
       <S.AddWrap>
-        <S.CloseButton />
-        <S.Title>날짜</S.Title>
-        <S.Date>2023.03.21</S.Date>
-        <S.Title>내용</S.Title>
-
-        <S.Input placeHolder="할 일을 입력하세요" />
-        <S.Msg></S.Msg>
-        <S.Button>등록</S.Button>
+        <S.CloseBox>
+          <S.CloseButton onClick={handleCloseModal} />
+        </S.CloseBox>
+        <S.DateBox>
+          <S.Title>날짜</S.Title>
+          <S.Date>2023.03.21</S.Date>
+        </S.DateBox>
+        <S.ContentBox>
+          <S.Title>내용</S.Title>
+          <S.Category>일상 취미 공부 기타</S.Category>
+          <S.Input type="text" placeholder="할 일을 입력해주세요." />
+          <S.Msg>내용을 입력해주세요!</S.Msg>
+        </S.ContentBox>
+        <S.Button onClick={handleAddTodoContent}>등록</S.Button>
       </S.AddWrap>
     </S.Container>
   );
---
+++ src/components/Todo/TodoInput/style.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import { BsX } from "react-icons/bs";
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  background: rgba(153, 153, 153, 0.2);
+  backdrop-filter: blur(10px);
+`;
+
+export const AddWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 33.0625rem;
+  height: 100%;
+  max-height: 30.875rem;
+  background-color: var(--color-background);
+  border-radius: 2.25rem;
+`;
+
+export const CloseBox = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+  /* height: 100%; */
+  padding: 2rem 2rem 0.625rem 0;
+`;
+export const CloseButton = styled(BsX)`
+  width: 2rem;
+  height: 2rem;
+  cursor: pointer;
+`;
+
+export const DateBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  max-width: 27.25rem;
+  margin-bottom: 2.25rem;
+  /* height: 100%; */
+`;
+
+export const Title = styled.label`
+  font-family: "SCoreDream-Medium";
+  font-size: 1rem;
+  line-height: 1.1875rem;
+`;
+
+export const Date = styled.label`
+  padding: 1.125rem 0 1.0625rem 1rem;
+  font-family: "SCoreDream-Light";
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  background-color: rgba(217, 217, 217, 0.15);
+  border-radius: 3rem;
+`;
+
+export const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  width: 100%;
+  max-width: 27.25rem;
+  height: 100%;
+  margin-bottom: 3.3175rem;
+`;
+
+export const Category = styled.label`
+  font-family: "SCoreDream-Medium";
+  font-size: 0.875rem;
+  line-height: 1.0625rem;
+  color: var(--color-text-hint);
+  margin-top: 0.25rem;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  /* height: 100%; */
+  padding: 1.125rem 0 1.0625rem 1rem;
+  font-family: "SCoreDream-Light";
+  color: var(--color-text-hint);
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  background-color: rgba(217, 217, 217, 0.15);
+  border-radius: 3rem;
+`;
+
+export const Msg = styled.label`
+  font-family: "SCoreDream-Light";
+  font-size: 0.75rem;
+  line-height: 0.875rem;
+  color: var(--color-text-warning);
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  max-width: 27.25rem;
+  height: 100%;
+  max-height: 3.375rem;
+  font-family: "SCoreDream-Medium";
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  color: #fff;
+  border-radius: 3rem;
+  background-color: var(--color-main);
+  margin-bottom: 43.92px;
+  cursor: pointer;
+`;
