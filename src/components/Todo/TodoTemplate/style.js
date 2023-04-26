76d9f83b3b37ef3a6382fe1efe27736a2a057104
yejz0715 <yejz0715@gmail.com>
2023-04-26 17:24:07 +0900

    todoTemplate_스타일 수정

--- src/components/Todo/TodoTemplate/style.js
+++ src/components/Todo/TodoTemplate/style.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 31.25rem;
-  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  width: 100%;
+  max-width: 31.25rem;
+  height: 100%;
+  margin-left: 1.5rem;
 `;
