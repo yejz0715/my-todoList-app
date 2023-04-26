b3498ba3f879a3a7c0385be1b7bf8170523d2c0f
yejz0715 <yejz0715@gmail.com>
2023-04-26 17:23:23 +0900

    todoItem_ui 수정

--- src/components/Todo/TodoItem/index.jsx
+++ src/components/Todo/TodoItem/index.jsx
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import modifyBtn from "../../../assets/todo-item-modify-btn.png";
 
 const TodoItem = () => {
-  const IsCheck = () => {};
-  const ShowModifyOption = () => {};
+  const [isTodoCheck, setTodoCheck] = useState(false);
+  const [isShowModal, setShowModal] = useState(false);
+  const [detailReadOnly, setDetailReadOnly] = useState(false);
+
+  const handleTodoCheck = () => {
+    setTodoCheck((prev) => !prev);
+    setDetailReadOnly((prev) => !prev);
+  };
+
+  const showModifyOption = () => {
+    setShowModal((prev) => !prev);
+  };
+
+  const handleUpdate = () => {
+    setTodoCheck((prev) => !prev);
+  };
   return (
-    <>
-      <S.Container>
-        <S.CheckBox />
-        <S.Contents>
-          <S.Category>개발</S.Category>
-          <S.Detail>todo 플젝 ui 완성</S.Detail>
-        </S.Contents>
-        <S.ModifyButton src={modifyBtn} alt="modifyBtn" />
-      </S.Container>
-
-      <S.Container>
-        <S.CheckBox />
-        <S.Contents>
-          <S.Category>개발</S.Category>
-          <S.Detail>todo 플젝 ui 완성</S.Detail>
-        </S.Contents>
-        <S.ModifyButton src={modifyBtn} alt="modifyBtn" />
-      </S.Container>
-
-      <S.Container>
-        <S.CheckBox onChange={IsCheck} />
-        <S.Contents>
-          <S.Category>개발</S.Category>
-          <S.Detail>todo 플젝 ui 완성</S.Detail>
-        </S.Contents>
+    <S.Container onClick={() => setShowModal(false)} isTodoCheck={isTodoCheck}>
+      <S.CheckBox onClick={handleTodoCheck} isTodoCheck={isTodoCheck} />
+      <S.Contents>
+        <S.Category>개발</S.Category>
+        <S.Detail
+          type="text"
+          isTodoCheck={isTodoCheck}
+          readOnly={detailReadOnly}
+        />
+      </S.Contents>
+      <S.ModifyWarp>
         <S.ModifyButton
           src={modifyBtn}
           alt="modifyBtn"
-          onClick={ShowModifyOption}
+          onClick={(e) => {
+            showModifyOption();
+            e.stopPropagation();
+          }}
         />
-        <S.ModifyBox>
-          <S.UpdateButton>수정</S.UpdateButton>
+        <S.ModifyBox isShowModal={isShowModal}>
+          <S.UpdateButton onClick={handleUpdate}>수정</S.UpdateButton>
           <S.DeleteButton>삭제</S.DeleteButton>
         </S.ModifyBox>
-      </S.Container>
-    </>
+      </S.ModifyWarp>
+    </S.Container>
   );
 };
 
--- src/components/Todo/TodoItem/style.js
+++ src/components/Todo/TodoItem/style.js
@@ -10,12 +10,16 @@ export const Container = styled.div`
   max-height: 4.4375rem;
   padding: 0 1.0725rem;
   margin-bottom: 1.25rem;
+  background-color: ${(props) =>
+    props.isTodoCheck === true ? "#f4f4f4" : "#fff"};
   border: 1px solid rgba(0, 0, 0, 0.1);
   border-radius: 1.25rem;
 `;
 export const CheckBox = styled.div`
   width: 1.6087rem;
   height: 1.5rem;
+  background-color: ${(props) =>
+    props.isTodoCheck === true ? "var(--color-main)" : "none"};
   border: 1px solid rgba(0, 0, 0, 0.1);
   border-radius: 0.25rem;
   cursor: pointer;
@@ -33,43 +37,54 @@ export const Category = styled.label`
   font-family: "SCoreDream-Light";
   color: var(--color-text-serve);
 `;
-export const Detail = styled.label`
+export const Detail = styled.input`
+  width: 100%;
+  height: 100%;
   font-size: 1rem;
   line-height: 1.1875rem;
+  color: ${(props) =>
+    props.detailReadOnly === true ? "var(--color-text-hint)" : "#333"};
+  text-decoration: ${(props) =>
+    props.detailReadOnly === true ? "line-through" : "none"};
 `;
 
 export const ModifyWarp = styled.div`
   display: flex;
+  position: relative;
 `;
 
 export const ModifyButton = styled.img`
-  width: 1.6087rem;
-  height: 1.5rem;
+  width: 100%;
+  height: 100%;
+  max-width: 1.6087rem;
+  max-height: 1.5rem;
   cursor: pointer;
 `;
 export const ModifyBox = styled.div`
-  display: flex;
+  display: ${(props) => (props.isShowModal === true ? "flex" : "none")};
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 0.625rem;
   position: absolute;
-  width: 100%;
-  max-width: 3.0625rem;
-  height: 100%;
-  max-height: 3.75rem;
+  top: -1.125rem;
+  left: 0.9375rem;
+  width: 3.0625rem;
+  height: 3.75rem;
+  z-index: 99;
   filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.1));
   background-color: #fff;
   border-radius: 0.25rem;
+  margin-right: 0.1875rem;
 `;
-export const UpdateButton = styled.div`
+export const UpdateButton = styled.label`
   font-size: 0.875rem;
   line-height: 1.0625rem;
   font-family: "SCoreDream-Light";
   color: var(--color-text-serve);
   cursor: pointer;
 `;
-export const DeleteButton = styled.div`
+export const DeleteButton = styled.label`
   font-size: 0.875rem;
   line-height: 1.0625rem;
   font-family: "SCoreDream-Light";
